d08e9f4baf545d98301d3e4fbedf17fc32234ad5
magdump <pirjademl7@gmail.com>
2024-05-04 11:16:29 +0530

    fix : add secondary fallback font which is used by acmelogo component

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,8 @@
-import { Montserrat  } from 'next/font/google'
-export const monte=Montserrat({subsets:['cyrillic']})
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
